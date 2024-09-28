27c9217acfb97147385cedcea90e07c7da70b0c9
Dhrumil Mehta <dhrumil.mehta@columbia.edu>
2024-09-28 03:47:42 -0400

    update link

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('submitPromptBtn').addEventListener('click', function() {
-    window.open('https://forms.gle/vNTn9c6HGx6UbdhFA', '_blank');
+    window.open('padlet.com/data4news/mediaparty', '_blank');
 });
